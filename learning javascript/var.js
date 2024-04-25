8cac4f6a1acab937bd6cbaf67a6b60ac27b1b599
Usman Fadil <usmanfadiloladele@gmail.com>
2024-04-25 18:01:01 +0100

    First commit

---
+++ learning javascript/var.js
@@ -0,0 +1,36 @@
+//console.clear();
+//var num1 = 5;
+//var num2 = 10;
+//var num3 = 15;
+
+//var sum = num1 + num2 + num3
+
+//var firstName = "Adebayo"
+
+//console.clear();
+
+var firstNum = 10;
+
+var secondNum =15;
+
+console.log(secondNum);
+
+//camel case (reccomended for Js)
+var firstNum;
+// pascal case
+var FirstNum;
+//snake case
+var first_num;
+
+
+// Rules for naming Js variables
+// - Name can contain letters, digits, _ and $
+var mySubject1, my_number, $400
+// - Name can not start with a digit
+// var 1mynumber
+// - Name can start with an - or $
+var _toMe, $good
+// - Names are case sensitive
+var xray, xray
+// - name cannot be reserved keywords
+// var varName;
